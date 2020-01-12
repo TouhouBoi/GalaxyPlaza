2c092c0e85580e70830efda4b4c60ea1b624337a
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2020-01-12 01:28:03 -0500

    Update complete-en.js

--- assets/js/complete-en.js
+++ assets/js/complete-en.js
@@ -5230,7 +5230,7 @@ Olv.Locale.Data = {
     },
     "olv.portal.report.report_spoiler %1": {
         "args": [],
-        "value": "Report Spoilers to Miiverse Administrators"
+        "value": "Report Spoilers to Galaxy Plaza Administrators"
     },
     "olv.portal.report.report_spoiler [_1]": {
         "args": [],
