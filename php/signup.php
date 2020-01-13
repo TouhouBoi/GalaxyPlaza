bdf52fcaa9f7f1da2c08bd62605c9800b4a779b8
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2020-01-12 19:19:29 -0500

    Cleanup signup.php

--- php/signup.php
+++ php/signup.php
@@ -15,92 +15,124 @@ if (ALLOW_SIGNUP == false)
 }
 
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if(!isset($_POST['token']) || $_SESSION['token'] !== $_POST['token']) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    if (!isset($_POST['token']) || $_SESSION['token'] !== $_POST['token'])
+	{
         $error = 'The CSRF check failed.';
         goto showForm;
     }
-    if(empty($_POST['username']) || empty($_POST['nickname']) || empty($_POST['password']) || empty($_POST['confirm_password'])) {
+	
+    if (empty($_POST['username']) || empty($_POST['nickname']) || empty($_POST['password']) || empty($_POST['confirm_password']))
+	{
         $error = 'You must fill out all fields.';
         goto showForm;
     }
-    if(!preg_match("/^[A-Za-z0-9-._]{1,32}$/", $_POST['username'])) {
+	
+    if (!preg_match("/^[A-Za-z0-9-._]{1,32}$/", $_POST['username']))
+	{
         $error = 'Your username is invalid.';
         goto showForm;
     }
+	
     $stmt = $db->prepare('SELECT COUNT(*) FROM users WHERE username = ?');
-    if(!$stmt) {
+	
+    if (!$stmt)
+	{
         $error = 'There was an error while preparing to search for users with your username.';
         goto showForm;
     }
+	
     $stmt->bind_param('s', $_POST['username']);
+	
     $stmt->execute();
-    if($stmt->error) {
+	
+    if ($stmt->error)
+	{
         $error = 'There was an error while searching for users with your username.';
         goto showForm;
     }
+	
     $result = $stmt->get_result();
+	
     $row = $result->fetch_array();
-    if($row['COUNT(*)'] > 0) {
+	
+    if ($row['COUNT(*)'] > 0)
+	{
         $error = 'A user already exists with that username.';
         goto showForm;
     }
-    if(mb_strlen($_POST['nickname']) > 64) {
+	
+    if (mb_strlen($_POST['nickname']) > 64)
+	{
         $error = 'Your nickname is too long.';
         goto showForm;
     }
-    if(!empty($_POST['email']) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	
+    if (!empty($_POST['email']) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+	{
         $error = 'Your email address is invalid.';
         goto showForm;
     }
-    if(!empty($_POST['email'])) {
+	
+    if (!empty($_POST['email']))
+	{
         $stmt = $db->prepare('SELECT COUNT(*) FROM users WHERE email = ? AND status = 0');
+		
         $stmt->bind_param('s', $_POST['email']);
+		
         $stmt->execute();
-        if($stmt->error) {
+		
+        if ($stmt->error)
+		{
             $error = 'An error occurred while checking for users with that email.';
             goto showForm;
         }
+		
         $result = $stmt->get_result();
+		
         $row = $result->fetch_assoc();
-        if($row['COUNT(*)'] > 0) {
+		
+        if ($row['COUNT(*)'] > 0)
+		{
             $error = 'An account already exists with that email.';
             goto showForm;
         }
     }
-    if(mb_strlen($_POST['password']) > 72)
+	
+    if (mb_strlen($_POST['password']) > 72)
 	{
         $error = 'Your password is too long.';
         goto showForm;
     }
 	
-    if($_POST['password'] !== $_POST['confirm_password'])
+    if ($_POST['password'] !== $_POST['confirm_password'])
 	{
         $error = 'Your password and confirm password do not match.';
         goto showForm;
     }
 	
-    if(!empty($_POST['nnid']) && !preg_match('/^[A-Za-z0-9-._]{6,16}$/', $_POST['nnid']))
+    if (!empty($_POST['nnid']) && !preg_match('/^[A-Za-z0-9-._]{6,16}$/', $_POST['nnid']))
 	{
         $error = 'Your Nintendo Network ID is invalid.';
         goto showForm;
     }
 	
-    if(!empty($_POST['nnid']))
+    if (!empty($_POST['nnid']))
 	{
         $ch = curl_init('https://pf2m.com/hash/' . urlencode($_POST['nnid']));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $miiHash = curl_exec($ch);
         $responseCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
         curl_close($ch);
-        if($responseCode < 200 || $responseCode > 299)
+        if ($responseCode < 200 || $responseCode > 299)
 		{
             $error = 'The Nintendo Network ID could not be found.';
             goto showForm;
         }
     }
 	
-    if(!empty(RECAPTCHA_SECRET))
+    if (!empty(RECAPTCHA_SECRET))
 	{
         $ch = curl_init('https://www.google.com/recaptcha/api/siteverify');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -115,22 +147,23 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 	
-    if(file_get_contents('https://check.getipintel.net/check.php?contact=' . (!empty(CONTACT_EMAIL) ? urlencode(CONTACT_EMAIL) : 'miiverseworld@reconmail.com') . '&flags=m&ip=' . urlencode($ip)) === '1')
+	$proxy_check = file_get_contents(
+		'https://check.getipintel.net/check.php?contact='.
+		(!empty(CONTACT_EMAIL) ? urlencode(CONTACT_EMAIL) : 'miiverseworld@reconmail.com').
+		'&flags=m&ip=' . urlencode($ip)
+	);
+	
+    if ($proxy_check === '1')
 	{
         $error = 'You cannot sign in using a proxy.';
         goto showForm;
     }
 
     $email = empty($_POST['email']) ? null : $_POST['email'];
-	
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-	
     $avatar = empty($miiHash) ? (!empty($_POST['email']) ? GRAVATAR_URL . md5($_POST['email']) . '?s=' . GRAVATAR_IMAGE_SIZE : null) : $miiHash;
-	
     $has_mh = empty($miiHash) ? 0 : 1;
-	
     $nnid = empty($_POST['nnid']) ? null : $_POST['nnid'];
-	
     $mh = empty($miiHash) ? null : $miiHash;
 	
     $stmt = $db->prepare('INSERT INTO users (username, nickname, email, password, avatar, has_mh, nnid, mh, ip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
@@ -175,13 +208,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $row = $result->fetch_array();
 	
     $_SESSION['id'] = $row['id'];
-	
     $_SESSION['username'] = $_POST['username'];
-	
     $_SESSION['avatar'] = $avatar;
-	
     $_SESSION['has_mh'] = $has_mh;
-	
     $_SESSION['level'] = 0;
 	
     $token = md5(uniqid());
